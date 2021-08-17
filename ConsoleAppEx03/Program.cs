37a0c926081601fbe7c63b2620de0203dac17ba2
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-17 20:26:26 -0300

    Introducao a POO - Ex_03

---
+++ ConsoleAppEx03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do retângulo:");
+            retangulo.Largura = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo);
+        }
+    }
+}
