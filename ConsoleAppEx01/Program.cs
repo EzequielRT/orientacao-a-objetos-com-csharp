a2f0202c96a6931d1a9182fdd9ccf85709797ffc
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-16 22:01:44 -0300

    Introducao a POO - Ex_01

---
+++ ConsoleAppEx01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleAppEx01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+           
+            Console.WriteLine("Digite os dados da 1ª pessoa:");
+            Console.Write("Nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa1.Idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados da 2ª pessoa:");
+            Console.Write("Nome: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa2.Idade = Convert.ToInt32(Console.ReadLine());
+
+            if (pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha {pessoa1.Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha {pessoa2.Nome}");
+            }
+        }
+    }
+}
