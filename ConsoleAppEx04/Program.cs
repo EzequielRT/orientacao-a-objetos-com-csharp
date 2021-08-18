926ed07db4aa32ba928c9c5e455f6c3c6098c986
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-17 21:11:54 -0300

    Introducao a POO - Ex_04

---
+++ ConsoleAppEx04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {funcionario}");
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+        }
+    }
+}
