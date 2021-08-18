70ce1fc6312e823e27f9d636b157cbee571fdc1d
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-17 21:51:14 -0300

    Introducao a POO - Ex_05

---
+++ ConsoleAppEx05/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx05
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota01, Nota02, Nota03, NotaFinal;
+
+        public void CalculaNotaFinal()
+        {
+            NotaFinal = Nota01 + Nota02 + Nota03;
+        }
+
+        public void VerificaSituacaoDoAluno()
+        {
+            if (NotaFinal < 60)
+            {
+                double pontosQueFaltou = 60.00 - NotaFinal;
+
+                Console.WriteLine($"NOTA FINAL = {NotaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"FALTARAM {pontosQueFaltou.ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+            else 
+            {
+                Console.WriteLine($"NOTA FINAL = {NotaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("APROVADO");
+            }
+        }
+    }
+}
---
+++ ConsoleAppEx05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota01 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota02 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota03 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            aluno.CalculaNotaFinal();
+            aluno.VerificaSituacaoDoAluno();
+        }
+    }
+}
