eee89163a6b7397798f8cc1ed41a941d19fef640
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-18 19:04:26 -0300

    Introducao a POO - Ex_06

---
+++ ConsoleAppEx06/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEx06
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double Converter(double cotacaoDolar, double qtdDeDolares)
+        {
+            double valorEmReais = cotacaoDolar * qtdDeDolares;
+            return valorEmReais + (valorEmReais * Iof / 100);
+        }
+    }
+}
---
+++ ConsoleAppEx06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qdtDeDolares = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"Valor a ser pago em reais = R$ {ConversorDeMoeda.Converter(cotacao, qdtDeDolares).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
