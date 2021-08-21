9ae3aa132b0f80d291080cf7f85c8a51313cfdf1
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-20 21:32:04 -0300

    Encapsulamento - Ex_07

---
+++ ConsoleAppEx07/ContaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx07
+{
+    class ContaBancaria
+    {
+        // Atributo
+        private string _titular;
+
+        // Propriedades
+        public int NmrDaConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        // Construtores
+        public ContaBancaria(string nome, int nmrDaConta)
+        {
+            this._titular = nome;
+            this.NmrDaConta = nmrDaConta;
+            this.Saldo = 0.00;
+        }
+
+        public ContaBancaria(string nome, int nmrDaConta, double depositoInicial) : this(nome, nmrDaConta)
+        {
+            Deposito(depositoInicial);
+        }
+
+        // Propriedade customizada
+        public string Nome
+        {
+            get { return this._titular; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    this._titular = value;
+                }
+            }
+        }
+
+        // Demais métodos da classe
+        public void Deposito(double valorDepositado)
+        {
+            this.Saldo += valorDepositado;
+        }
+
+        public void Saque(double valorDoSaque)
+        {
+            this.Saldo -= (valorDoSaque + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return
+                $"Conta {this.NmrDaConta}, Titular: {this._titular}, Saldo: $ {this.Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ ConsoleAppEx07/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleAppEx07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int nmrDaConta = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Entre com o nome do titular: ");
+            string nomeDoTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = Convert.ToChar(Console.ReadLine());
+
+            if (resp.ToString().ToLower() == "s")
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double depositoInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(nomeDoTitular, nmrDaConta, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(nomeDoTitular, nmrDaConta);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double quantia = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
