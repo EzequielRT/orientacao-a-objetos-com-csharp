7f5ab49829d6f65c4551a40dc5f107f510f16f16
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-22 18:34:20 -0300

    Vetores - Ex_08

---
+++ ConsoleAppEx08/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppEx08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] quartoAlugado = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int qtdDeQuartosAlugados = Convert.ToInt16(Console.ReadLine());
+            
+            for (int i = 1; i <= qtdDeQuartosAlugados; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int nmrQuarto = Convert.ToInt16(Console.ReadLine());
+
+                quartoAlugado[nmrQuarto] = new Estudante { Nome = nome, Email = email };
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < quartoAlugado.Length; i++)
+            {
+                if (quartoAlugado[i] != null)
+                {
+                    Console.WriteLine($"{i}: {quartoAlugado[i]}");
+                }
+            }
+
+        }
+    }
+}
