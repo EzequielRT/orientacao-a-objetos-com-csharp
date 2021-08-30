f06b17033b2e3ae0f938ca1a7e73936c71d9d92a
EzequielRT <ezefuts@gmail.com>
2021-08-30 00:22:41 -0300

    Composicao e StringBuilder - Ex_11

---
+++ ConsoleAppEx11/Entities/Order.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleAppEx11.Entities.Enums;
+using System.Globalization;
+
+namespace ConsoleAppEx11.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem order in Items)
+            {
+                sum += order.SubTotal();                
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("ORDER SUMARY:");
+            sb.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate.ToString("dd/MM/yyyy")}) - {Client.Email}");
+            sb.AppendLine($"Order items: ");
+            foreach  (OrderItem item in Items)
+            {
+                sb.Append($"{item.Product.Name}, ${item.Product.Price.ToString("F2", CultureInfo.InvariantCulture)}, ");
+                sb.AppendLine($"Quantity: {item.Quantity}, Subtotal: ${item.SubTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            };
+            sb.AppendLine($"Total price: ${Total().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleAppEx11/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleAppEx11.Entities;
+using ConsoleAppEx11.Entities.Enums;
+using System.Globalization;
+using System;
+
+namespace ConsoleAppEx11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = Convert.ToDateTime(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus os = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, os, client);
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodPrice = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+
+
+                Product product = new Product(prodName, prodPrice);
+
+                OrderItem orderItem = new OrderItem(quantity, prodPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(order);
+
+        }
+    }
+}
