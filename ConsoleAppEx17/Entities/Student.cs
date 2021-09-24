6277a14d796250f8fd7ad800544669764e7196ec
EzequielRT <ezefuts@gmail.com>
2021-09-23 21:40:42 -0300

    Conjunto HashSet - Ex_17

---
+++ ConsoleAppEx17/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleAppEx17.Entities
+{
+    class Student
+    {
+        public int Cod { get; private set; }
+
+        public Student(int plate)
+        {
+            Cod = plate;
+        }
+
+        public override int GetHashCode()
+        {
+            return Cod.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return Cod.Equals(other.Cod);
+        }
+    }
+}
---
+++ ConsoleAppEx17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ConsoleAppEx17.Entities;
+using System.Collections.Generic;
+
+namespace ConsoleAppEx17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> totalStudents = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int qttA = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < qttA; i++)
+            {
+                int cod = Convert.ToInt32(Console.ReadLine());
+                totalStudents.Add(new Student(cod));
+            }
+
+            Console.Write("How many students for course B? ");
+            int qttB = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < qttB; i++)
+            {
+                int cod = Convert.ToInt32(Console.ReadLine());
+                totalStudents.Add(new Student(cod));
+            }
+
+            Console.Write("How many students for course C? ");
+            int qttC = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < qttC; i++)
+            {
+                int cod = Convert.ToInt32(Console.ReadLine());
+                totalStudents.Add(new Student(cod));
+            }
+
+            Console.WriteLine($"Total students: {totalStudents.Count}");
+        }
+    }
+}
